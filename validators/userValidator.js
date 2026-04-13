b892034b7ee247bff7de13f3e4e5bb11fdaee18c
BexeitAlua <aluabekseit@gmail.com>
2026-04-13 11:04:36 +0500

    bug fixing

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,20 +1,35 @@
 const Joi = require('joi');
 
-const userRegistrationSchema = Joi.object({
-    name: Joi.string().min(2).max(50).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).max(100).required()
-});
-
-const userLoginSchema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required()
-});
-
-const validateUserRegistration = (data) => userRegistrationSchema.validate(data);
-const validateUserLogin = (data) => userLoginSchema.validate(data);
-
-module.exports = {
-    validateUserRegistration,
-    validateUserLogin
+const validateUserRegistration = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().min(2).max(50).required().messages({
+            'string.min': 'Name must be at least 2 characters',
+            'string.max': 'Name must be less than 50 characters',
+            'any.required': 'Name is required'
+        }),
+        email: Joi.string().email().required().messages({
+            'string.email': 'Please enter a valid email',
+            'any.required': 'Email is required'
+        }),
+        password: Joi.string().min(6).required().messages({
+            'string.min': 'Password must be at least 6 characters',
+            'any.required': 'Password is required'
+        })
+    });
+    return schema.validate(data);
 };
+
+const validateUserLogin = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().min(2).max(50).required().messages({
+            'string.min': 'Username must be at least 2 characters',
+            'any.required': 'Username is required'
+        }),
+        password: Joi.string().required().messages({
+            'any.required': 'Password is required'
+        })
+    });
+    return schema.validate(data);
+};
+
+module.exports = { validateUserRegistration, validateUserLogin };
